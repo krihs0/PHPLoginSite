591f01887320c8647441d97f6dd264d3ba2de1ee
kris <kris@webvalue.nl>
2023-06-22 16:51:16 +0200

    naar

--- footer.php
+++ footer.php
@@ -1,2 +1,11 @@
+
+
+    <footer class="footer">
+
+        <div class="footer__bottom">
+            <p>socials - soon</p>
+            <p>&copy; <script>document.write(new Date().getFullYear())</script></p>
+        </div>
+    </footer>
 </body>
 </html>
---
+++ profile.php
@@ -0,0 +1,9 @@
+<?php
+include_once 'header.php';
+?>
+<section class="profile">
+
+</section>
+<?php
+include_once 'footer.php';
+?>
